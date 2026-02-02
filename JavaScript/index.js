ef0fef350d6d0fbdae5618c00631aecb40789dca
Talhah Zahid <talhazahid415@gmail.com>
2026-02-02 13:03:53 +0500

    practise array method

---
+++ JavaScript/index.js
@@ -0,0 +1,73 @@
+// console.log("hello javascript");
+
+// console.log(this.window);
+
+// const number = [1, 2, 3, 4];
+// const newNumber = number.map((item) => {
+//   return "num-" + item;
+// });
+
+// console.log(newNumber);
+
+// const nums = [5, 12, 8, 130, 44];
+// const newNumber = nums.filter((item) => {
+//   return item > 10 && item % 2 == 0;
+// });
+// console.log(newNumber)
+
+// const users = [
+//   { id: 1, name: "Ali" },
+//   { id: 2, name: "Sara" },
+// ];
+
+// const newUser = users.find((item) => {
+//   return item.id === 2;
+// });
+// console.log(newUser);
+
+// const num = [1, -2, 4, 6, -4, 5, 34];
+// const newNumber = num.some((item) => {
+//   return item < 0;
+// });
+// console.log(newNumber);
+
+// const num = [1, 2, 3, 5, 646, 46747];
+// const newNumber = num.every((item) => item > 0);
+// console.log(newNumber);
+
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const newNumber = num.includes(2);
+// console.log(newNumber);
+
+// const num = [10, 2, 5, 1];
+// const newNumber = num.sort((item) => {
+//   return item;
+// });
+// console.log(newNumber);
+
+// const num = [1, 2, 2, 3, 4, 4, 5];
+// const newNumber = [...new Set(num)];
+// console.log(newNumber);
+
+// const num = [1, 2, 2, 3, 4, 4, 5];
+// const newNumber = num.filter((item, index, arr) => {
+//   return arr.indexOf(item) === index;
+// });
+
+// console.log(newNumber);
+
+// const cart = [
+//   { price: 100, qty: 2 },
+//   { price: 50, qty: 3 },
+// ];
+
+// const totalPrice = cart.map((item) => {
+//   return item.price * item.qty;
+// });
+// console.log(totalPrice);
+
+// const num = [10, 40, 3, 5, 12, 42];
+// const res1 = num.sort((a, b) => a - b); // ascending
+// const res2 = num.sort((a, b) => b - a); // descending
+// console.log(res1);
+// console.log(res2);
